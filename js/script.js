0234d27cdba9e6f7d10b746b4f9e23880fbf3237
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-05-12 00:34:11 -0300

    remove

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){    
-    $(window).on('scroll', function(){
-        if($(window).scrollTop()){
-            $(#menu).addClass('ativo');
-        }else{
-            $(#menu).removeClass('ativo');
-        }
-    })
